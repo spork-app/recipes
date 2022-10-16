8fdadf99227f4e5b14abe0e40d1398657c91331f
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-10-15 23:31:24 -0400

    Fix PHPUnit.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,7 @@ namespace Spork\Food\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as TestbenchTestCase;
-use Spork\Food\FoodServiceProvider.php;
+use Spork\Food\FoodServiceProvider;
 
 class TestCase extends TestbenchTestCase
 {
@@ -20,7 +20,7 @@ class TestCase extends TestbenchTestCase
     protected function getPackageProviders($app)
     {
         return [
-            FoodServiceProvider.php::class,
+            FoodServiceProvider::class,
         ];
     }
 }
